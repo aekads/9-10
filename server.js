a3eee754ca01f476fddec3423a6651d7869a41ab
aekads <aekads.in@gmail.com>
2024-12-20 16:41:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -786,7 +786,7 @@ setInterval(() => {
   });
 
   console.log('Scheduled VIDEO_IMPRESSION command sent to all connected clients');
-}, 600000); // 3600000 milliseconds = 1 hour
+}, 60000); // 3600000 milliseconds = 1 hour
 
 
 
