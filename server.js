7a969ffa2678eaf8f763b6d5123fe45a3e39dfd6
aekads <aekads.in@gmail.com>
2025-01-22 12:16:01 +0000

    Updated all modified files with the latest changes

--- server.js
+++ server.js
@@ -760,7 +760,7 @@ app.get('/video-impressions', async (req, res) => {
       LEFT JOIN 
         public.screens AS s
       ON 
-           vi.screen_id::text= s.screenid::integer  -- Cast both to compatible type
+           vi.screen_id= s.screenid  -- Cast both to compatible type
       ORDER BY 
         vi.uploaded_date DESC; -- Change DESC to ASC for ascending order
     `;
